ac7f3ff0f8ed72dec6ac484347cde96468e92bc3
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-03-21 16:35:23 +0100

    [UPD] Controllo se  il  numero  è  pari

--- script.js
+++ script.js
@@ -8,12 +8,27 @@ function palindroma(parola) {
 console.log(palindroma(parola));
 
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
-
-let  scelta = prompt('Scegli pari o dispari?');
-let numero =  parseInt(prompt('Inserisci un numero da 1 a 5'));
-function numeroRandom (min,max){
-    return Math.floor(Math.random() * (max - min + 1) + min)
+let scelta = prompt('Scegli pari o dispari?');
+let numero = parseInt(prompt('Inserisci un numero da 1 a 5'));
+//genero per il computer
+function numeroRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
-let  numeroComputer  =  numeroRandom(1,5);
+let numeroComputer = numeroRandom(1, 5);
 console.log(numeroComputer);
+//sommo i due valori numerici
+function sommaNumeri(numero, numeroComputer) {
+    return numero + numeroComputer
+}
+//controllo se è pari così da poter stabilire il vincitore
+if (sommaNumeri(numero, numeroComputer) %= 2 === 0) {      
+    console.log('pari')
+} else{
+    console.log('dispari')
+}
+
+
+
+
+
 
