cda0e9b8376c0b4df055a4a8f4bfc82f5907bcbc
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-03-21 16:12:43 +0100

    [UPD] numero  e console utente

--- script.js
+++ script.js
@@ -7,4 +7,13 @@ function palindroma(parola) {
 }
 console.log(palindroma(parola));
 
-//
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+let  scelta = prompt('Scegli pari o dispari?');
+let numero =  parseInt(prompt('Inserisci un numero da 1 a 5'));
+function numeroRandom (min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+let  numeroComputer  =  numeroRandom(1,5);
+console.log(numeroComputer);
+
